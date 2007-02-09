a1dd30232860689cd725272d2673f466a1be444d
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-09 19:51:45 +0000

    --updated getters and setters.

--- trunk/teddySoft/User.java
+++ trunk/teddySoft/User.java
@@ -11,5 +11,23 @@ public class User {
 		return name;
 	}
 	
+	public void setPassword(String newPass){
+		password = newPass;
+	}
+	
+	public Database getDatabase(){
+		return db;
+	}
+	
+	public void setName(String newName){
+		name = newName;
+	}
+	
+	
+	public User(String newName, String newPassword){
+			password = newPassword;
+			name = newName;
+			db = new Database();
+	}
 	
 }
