19d595a8941a545fc72a54fd62817700037d48b6
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-09 19:41:24 +0000

    --Modified a few things

--- teddySoft/PassGen.java
+++ teddySoft/PassGen.java
@@ -1,5 +1,16 @@
 package teddySoft;
 
 public class PassGen {
-
+	
+	private String password;
+	
+	
+	public String genPass(){
+		password = "";
+		// modify string here
+		return password;
+	}
+	
+	
+	
 }
