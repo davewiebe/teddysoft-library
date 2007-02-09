6f1bb157262c05c32a198543de7cd8867a09ea97
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-09 19:41:24 +0000

    --Modified a few things

--- trunk/teddySoft/Books.java
+++ trunk/teddySoft/Books.java
@@ -1,5 +1,30 @@
 package teddySoft;
 
 public class Books {
-
+	private
+		String title;
+		String author;
+		String edition;
+		int isbn;
+		String pubdate;
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public String getEdition(){
+		return edition;
+	}
+	
+	public int getIsbn(){
+		return isbn;
+	}
+	
+	public String getPubdate(){
+		return pubdate;
+	}
 }
