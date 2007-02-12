061014d98a05ffbeeffa3bccf5956051066368a3
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-12 20:43:24 +0000

    --keygenerator up and running

---
+++ trunk/teddySoft/KeyGen.java
@@ -0,0 +1,41 @@
+package teddySoft;
+
+public class KeyGen {
+	public static String generateKey(String username, String password){
+
+		String key = "";
+		int u = 0;
+		int p = 0;
+		for (int i = 0; i < (username.length() + password.length()-2); i++){
+			if (u<username.length() && p<password.length()){
+				if(i%2==1){
+					key+= (char)(password.charAt(p)+i+(p+1%u+1));
+					p++;
+				}else{
+					key+= (char)(username.charAt(u)+i+((u+1)%(p+1)));
+					
+					u++;
+				}
+			}
+			if(p>=password.length()){
+				key+= (char)(username.charAt(u)+i+((p+1)%(u+1)));
+				
+				u++;
+			}
+			if(u>=username.length()){
+				key+= (char)(password.charAt(p)+i+((u+1)%(p+1)));
+				
+				p++;
+			}
+		}
+		System.out.println("key = "+key);
+		return key;
+	}
+	
+	public static void main(String[] args){
+		generateKey("dave", "jordan");
+	}
+	
+	
+	
+}
--- trunk/teddySoft/SecurityInterface.java
+++
@@ -1,8 +0,0 @@
-package teddySoft;
-
-public interface SecurityInterface {
-	void changePassword(String newPass);
-	boolean changeUserName(String newName);
-	boolean addUser(String name, String password);
-	
-}
