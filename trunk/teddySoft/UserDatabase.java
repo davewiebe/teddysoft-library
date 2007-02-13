dad07e17b5827a077dd3cb0f7b1406528ef1b641
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-13 20:23:18 +0000

    --User Database slightly functional!

--- trunk/teddySoft/UserDatabase.java
+++ trunk/teddySoft/UserDatabase.java
@@ -30,11 +30,24 @@ public class UserDatabase {
 	}
 	
 	public User findUser(String name){
-		return userList[0];
+		for (int i = 0; i < userListSize; i++){
+			if (userList[i].getName() == name){
+				return userList[i];
+			}
+		}
+		return null;
 	}
 	
 	public boolean addUser(User user){
-		
+		if (userListSize == userList.length){
+			User[] temp = new User[userListSize*2];
+			for (int i =0; i>userListSize; i++){
+				temp[i] = userList[i];
+			}
+			userList = temp;
+		}
+		userList[userListSize] = user;
+		userListSize++;
 		return true;
 	}
 
