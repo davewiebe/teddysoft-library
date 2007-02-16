5242445568f46cb39a9536d6f7966d0a4104c39e
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-02-16 12:24:52 +0000

    -- added view book functionality!

--- teddySoft/ViewBookGUI.java
+++ teddySoft/ViewBookGUI.java
@@ -314,7 +314,7 @@ public class ViewBookGUI implements ActionListener {
 
 	}	
 	/*
-	public static void main(String[] args){
+	public static void Test(){
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
