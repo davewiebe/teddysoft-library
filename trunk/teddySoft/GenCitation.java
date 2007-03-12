22c015dc49481d5a65e25d7064fe6606997ca028
dawiebe <dawiebe@a6925f54-4127-0410-8222-1df88af50054>
2007-03-12 19:06:30 +0000

    - generate citations class up and running.

--- trunk/teddySoft/GenCitation.java
+++ trunk/teddySoft/GenCitation.java
@@ -17,9 +17,30 @@ public class GenCitation {
 
 	
 	public String genAPA(String author, String year, String title, String place, String publisher, String edition){
-		String apa = citeAuthor(author) + citeYear(year) + 
-			citeTitle(title) + citePlace(place) + citePublisher(publisher) + citeEdition(edition);
-		return apa;
+	
+		
+		String apa = "";
+		String citeauthor = citeAuthor(author);
+		if (citeauthor.compareTo("") == 0){
+			apa = apa +
+				citeTitle(title) +
+				citeEdition(edition) +
+				citeYear(year) +
+				citePlace(place) +
+				citePublisher(publisher);
+			// return citation without author
+			return apa;
+		}
+		else {
+			apa = apa +
+				citeAuthor(author) + 
+				citeYear(year) + 
+				citeTitle(title) + 
+				citeEdition(edition) +
+				citePlace(place) + 
+				citePublisher(publisher);
+			return apa;
+		}
 	}
 
 	
@@ -34,21 +55,21 @@ public class GenCitation {
 		/*run through name.
 		if jr. exists, remove it, and add 1 to junior.
 
-		set first real value to be first name, unless followed by a comma.
-		set second real value to be last name, unless its one letter long, or one letter followed by a period.
-		if the second value was 1 letter long or followed by a period, set it to be the middle name, and set the third value 
-
-		to be the last name.
-
-
-		if first real value was followed by a comma, set the second value to be the first name, and the first value to be 
-
-		last name.
-		if the second value was 1 letter long or followed by a period, set it to be the middle name, and set the third value 
-
-		to be the last name.
-		*/
-		int h = 0;
+		else set first value to be first name, unless followed by a comma.
+		
+		if first value was first name:
+			set second value to be last name
+				if there is another value
+			 		set the middle to be the last name
+			 		set the third value to be the last name.
+			 		
+		if first value was followed by a comma,
+			set last name to be first value
+			remove the comma
+			set second value to be first name, unless followed by comma
+					
+		
+			*/		int h = 0;
 		String fname = "";
 		String mname = "";
 		String lname = "";
@@ -57,12 +78,13 @@ public class GenCitation {
 			splitString[i] = splitString[i].trim();
 			
 			if(splitString[i].compareToIgnoreCase("Jr.") == 0 ||
-					splitString[i].compareToIgnoreCase("Jr") == 0){
+					splitString[i].compareToIgnoreCase("Jr") == 0 ||
+					splitString[i].compareToIgnoreCase("Jr,") == 0){
 				jr = true;
 			}
 			
 			else if(h==0 && splitString[i].compareTo("") != 0){
-				if(splitString[i].endsWith(",") == true){
+				if(splitString[i].endsWith(",") == true){ // if firt value ends with comma
 					lname = splitString[i].substring(0, splitString[i].length()-1);
 					//System.out.println("1lname = "+lname);
 					h++;
@@ -73,38 +95,64 @@ public class GenCitation {
 				}
 			}
 			
-			else if(h==1 && fname.compareTo("") != 0){ // we already have a first name
-				if(splitString[i].compareTo("") != 0){
-					if(splitString[i].length() == 1 || 
-							(splitString[i].length() == 2 && splitString[i].endsWith("."))){
-						mname = (""+splitString[i].charAt(0));
-						//System.out.println("3mname = "+mname);
-						h++;
-					}
-					else{
-						lname = splitString[i];
-						//System.out.println("4lname = "+lname);
-						h++;
-					}
+			else if(h==1 && fname.compareTo("") != 0 &&
+					splitString[i].compareTo("") != 0){ // we already have a first name
+				if(splitString[i].endsWith(",") == false){
+					mname = splitString[i];
+					//System.out.println("3mname = "+mname);
+					h++;
 				}
-			}else{ // we need a first name
-				if(splitString[i].compareTo("") !=0){
-					if(splitString[i].length() == 1 || 
-							(splitString[i].length() == 2 && splitString[i].endsWith("."))){
-						mname = (""+splitString[i].charAt(0));
-						//System.out.println("5mname = "+mname);
-						h++;
-					}
-					else{
-						fname = splitString[i];
-						//System.out.println("6fname = "+fname);
-						h++;
-					}
+				else{
+					lname = splitString[i];
+					//System.out.println("4lname = "+lname);
+					h++;
 				}
+			}else if (h==2 && fname.compareTo("") != 0 &&
+					mname.compareTo("") != 0 && splitString[i].compareTo("") != 0){ // if we have a middle and first name
+				if (mname.endsWith(",") == false){ // if the middle really is the middle, and we have another value
+					lname = splitString[i];
+					h++;
+				}else {
+					mname = lname;
+					lname = splitString[i];
+				}
+				h++;
+				
+			}else if (h==2 && fname.compareTo("") != 0 &&
+					lname.compareTo("") != 0 && splitString[i].compareTo("") != 0){ // if we have a middle and first name
+				
+				mname = splitString[i];
+				h++;
+				
+			}else if (h==1 && lname.compareTo("") != 0 && // we have a last name, need a first name
+					splitString[i].compareTo("") !=0){
+				if((splitString[i].length() == 2 && splitString[i].endsWith(",")) ||
+						(splitString[i].length() == 2 && splitString[i].endsWith("."))){
+					mname = splitString[i];
+					//System.out.println("5mname = "+mname);
+					h++;
+				}
+				else{
+					fname = splitString[i];
+					//System.out.println("6fname = "+fname);
+					h++;
+				}				
+			}else if (h==2 && lname.compareTo("") != 0 &&
+					mname.compareTo("") != 0 && splitString[i].compareTo("") != 0){ // if we have a middle and first name
+				fname = splitString[i];
+				h++;
+				
+			}else if (h==2 && fname.compareTo("") != 0 &&
+					lname.compareTo("") != 0 && splitString[i].compareTo("") != 0){ // if we have a middle and first name
+				mname = splitString[i];
+				h++;
 			}
 			
 		}
-		
+		if (mname.compareTo("") != 0 && fname.compareTo("") == 0){
+			fname = mname;
+			mname = "";
+		}
 		if (lname.compareTo("") == 0){ // fixes bug where Williamson F  Jr. == , W. F., Jr.
 			lname = fname;
 			fname = mname;
@@ -113,21 +161,22 @@ public class GenCitation {
 		String name = "";
 		int period = 0;
 
-		name = name + lname;
+		name = name + ""+lname;
 		if (fname.compareTo("") != 0){
 			name = name + ", "+fname.charAt(0)+".";
 			period++;
 		}
 
 		if (mname.compareTo("") != 0){
-			name = name + ", "+mname.charAt(0)+".";
+			name = name + " "+mname.charAt(0)+".";
 			period++;
 		}
-		if(jr = true){
+		if(jr == true){
 			name = name + ", Jr.";
 			period++;
 		}
 		if (period == 0){ name = name+".";}
+		name = name + " ";
 		return name;
 	}
 	
@@ -135,13 +184,30 @@ public class GenCitation {
 		System.out.println(author + " == " + citeAuthor(author));
 	}
 	
-	public static void main(String[] args){
+	public static void main(String []args){
+		testCiteAuthor();
+		
+	}
+	
+	private static void testCiteAuthor(){
 		citeAuthorTest("Fanna  Williamson   Jr.");
-		citeAuthorTest("Williamson, Fanna  jr");
-		citeAuthorTest("Fanna JR. Williamson ");
-		citeAuthorTest("Williamson F  Jr.");
-		citeAuthorTest("Williamson, F  Jr.");
-		citeAuthorTest("Williamson, jr F");
+		citeAuthorTest("Williamson, Fanna  jr  ");
+		citeAuthorTest("Fanna JR. Williamson   ");
+		citeAuthorTest("Williamson F  Jr.      ");
+		citeAuthorTest("Williamson, F  Jr.     ");
+		citeAuthorTest("Williamson, jr F       ");
+		
+		citeAuthorTest("");
+		citeAuthorTest("Fds sfds fds fsdf dsf df");
+		citeAuthorTest("David Andrew Wiebe");
+		citeAuthorTest("04593 9305 39053943");
+		
+		citeAuthorTest("Fanna A. Williamson   Jr. ");
+		citeAuthorTest("Williamson, Fanna A jr    ");
+		citeAuthorTest("Fanna A JR. Williamson    ");
+		citeAuthorTest("F A Williamson Jr.        ");
+		citeAuthorTest("Williamson, F A.  Jr.     ");
+		citeAuthorTest("Williamson, A, jr F       ");
 	}
 	
 	private String citeYear(String year){
@@ -149,27 +215,38 @@ public class GenCitation {
 			return "";
 		}
 		else {
-			return ("("+year+").");
+			return ("("+year+"). ");
 		}
 	}
 	
 	private String citeTitle(String title){		
-		if(title.endsWith("."))
+		if(title.endsWith(". "))
 			return title;
 		else
-			return (title+".");
+			return (title);
 	}
 	
 	private String citePlace(String place){
-		return place+".";
+		return place;
 	}
 	
 	private String citePublisher(String publisher){
-		return publisher+".";
+		
+		if (publisher.compareTo("") != 0 || publisher.compareTo(" ") != 0){
+			return publisher+": ";
+		}
+		else{
+			return "";
+		}
 	}
 
 	private String citeEdition(String edition){
-		return edition+".";
+		if (edition.compareTo("") != 0 || edition.compareTo(" ") != 0){
+			return "("+edition+".). ";
+		}
+		else{
+			return "";
+		}
 	}
 	
 }
